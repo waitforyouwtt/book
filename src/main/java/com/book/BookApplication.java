548cb4c3f61dd5efd9fe5cf4ce4c40d5a9f2a3d0
luoxian <15290810931@163.com>
2018-10-09 23:25:22 +0800

    多文件的上传和下载

--- src/main/java/com/book/BookApplication.java
+++ src/main/java/com/book/BookApplication.java
@@ -3,16 +3,36 @@ package com.book;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+
+import javax.servlet.MultipartConfigElement;
 
 @ComponentScan(basePackages = "com")
 @MapperScan({"com.book.mapper","com.book.dao"})
 @ImportResource("quartz.xml")
 @SpringBootApplication
+@Configuration
 public class BookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookApplication.class, args);
+	}
+
+	/**
+	 * 文件上传配置
+	 * @return
+	 */
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//单个文件最大  //KB,MB
+		factory.setMaxFileSize("10240KB");
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("102400KB");
+		return factory.createMultipartConfig();
 	}
 }
---
+++ src/main/java/com/book/controller/FileController.java
@@ -0,0 +1,202 @@
+package com.book.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.List;
+
+/**
+ * @Author: 一点点
+ * @Date: 2018/10/9 22:36
+ * @Version 1.0
+ * 文件上传和下载
+ */
+@Controller
+@RequestMapping("/file")
+public class FileController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * 前往多文件上传和下载
+     * @return
+     */
+    @RequestMapping("/fileDownload")
+    public String fileDownload() {
+        return "fileDownload";
+    }
+    /**
+     * 文件上传相关代码
+     */
+    @RequestMapping(value = "upload")
+    @ResponseBody
+    public String upload(@RequestParam("test") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "文件为空";
+        }
+        // 获取文件名
+        String fileName = file.getOriginalFilename();
+        logger.info("上传的文件名为：" + fileName);
+        // 获取文件的后缀名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        logger.info("上传的后缀名为：" + suffixName);
+        // 文件上传后的路径
+        String filePath = "E://test//";
+        // 解决中文问题，liunx下中文路径，图片显示问题
+        // fileName = UUID.randomUUID() + suffixName;
+        File dest = new File(filePath + fileName);
+        // 检测是否存在目录
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+
+    /**
+     *  文件下载相关代码
+     */
+    @RequestMapping("/adownload")
+    public String adownloadFile(org.apache.catalina.servlet4preview.http.HttpServletRequest request, HttpServletResponse response){
+        String fileName = "E:\\timg.jpg";
+        if (fileName != null) {
+            //当前是从该工程的WEB-INF//File//下获取文件(该目录可以在下面一行代码配置)然后下载到C:\\users\\downloads即本机的默认下载的目录
+            String realPath = request.getServletContext().getRealPath("//WEB-INF//");
+            File file = new File(realPath, fileName);
+            if (file.exists()) {
+                // 设置强制下载不打开
+                response.setContentType("application/force-download");
+                // 设置文件名
+                response.addHeader("Content-Disposition","attachment;fileName=" +  fileName);
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    System.out.println("success");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    @RequestMapping("/download")
+    public String downLoad(HttpServletResponse response){
+        String filename="timg.jpg";
+        String filePath = "E:\\" ;
+        File file = new File(filePath + "/" + filename);
+        //判断文件父目录是否存在
+        if(file.exists()){
+            response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" + filename);
+
+            byte[] buffer = new byte[1024];
+            //文件输入流
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            //输出流
+            OutputStream os = null;
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    /**
+     * 实现多文件上传[两种方式都可以]
+     * */
+    @RequestMapping(value="/multifileUpload",method=RequestMethod.POST)
+   // public @ResponseBody String multifileUpload(@RequestParam("files")List<MultipartFile> files){
+    public @ResponseBody String multifileUpload(HttpServletRequest request){
+
+        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("files");
+
+        if(files.isEmpty()){
+            return "false";
+        }
+
+        String path = "E:/test" ;
+
+        for(MultipartFile file:files){
+            String fileName = file.getOriginalFilename();
+            int size = (int) file.getSize();
+            System.out.println(fileName + "-->" + size);
+
+            if(file.isEmpty()){
+                return "false";
+            }else{
+                File dest = new File(path + "/" + fileName);
+                //判断文件父目录是否存在
+                if(!dest.getParentFile().exists()){
+                    dest.getParentFile().mkdir();
+                }
+                try {
+                    file.transferTo(dest);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    return "false";
+                }
+            }
+        }
+        return "true";
+    }
+
+}
